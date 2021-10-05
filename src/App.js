9d2b7ce6f6eda25246384af59c7efe6d8af9fb8e
Alex <80081422+AlexSklyar@users.noreply.github.com>
2021-10-05 20:30:04 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import './App.css';
+import {First} from './First'
+
+
 
 export default function App() {
+ 
+  let someList = [
+    { id: 1, text: 'Animals' },
+    { id: 2, text: 'Anime' },
+    { id: 3, text: 'Anti-Malware' },
+    { id: 4, text: 'Art Design' },
+    { id: 5, text: 'Books' },
+    { id: 6, text: 'Business' },
+    { id: 7, text: 'Calendar' },
+    { id: 8, text: 'Cloud Storage' },
+    { id: 9, text: 'File Sharing' },
+    { id: 10, text: 'Animals' },
+    { id: 11, text: 'Continuous Integration' },
+    { id: 12, text: 'Cryptocurrency' }
+  ];
   return (
-    <div>
-      Some data:
+      <div>
+      Some List:
+      
+        <First list={someList.map(el => el.toLowerCase())}/>
+    
     </div>
   );
 }
